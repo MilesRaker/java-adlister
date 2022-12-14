95efdae63d2b2191fcdefe3e42737b21db1430e6
Miles Raker <milesraker@gmail.com>
2022-12-14 13:11:44 -0800

    servlet connection established, state is maintained between page loads

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -3,18 +3,21 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+
+    protected int count = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String userName = request.getParameter("name");
-        if(userName != null){
-            out.println("<h1>Hello, " + userName + "</h1>");
-        } else {
-            out.println("<h1>Hello</h1>");
+        boolean reset = Boolean.parseBoolean(request.getParameter("reset"));
+        if(reset){
+            count = 0;
+        }else {
+            count++;
         }
+        out.println("<p>COUNT: " + count);
     }
 }
