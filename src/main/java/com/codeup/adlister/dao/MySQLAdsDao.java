bdd5d9f431c071fe4bda6a6235ba5c9d7dc0ced1
Miles Raker <milesraker@gmail.com>
2022-12-29 19:49:49 -0800

    comparing to functional repo

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,11 +1,7 @@
 package com.codeup.adlister.dao;
-
+import com.codeup.adlister.controllers.Config;
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,13 +12,17 @@ public class MySQLAdsDao implements Ads {
     public MySQLAdsDao(Config config) {
         try {
             DriverManager.registerDriver(new Driver());
+        } catch(SQLException e){
+            throw new RuntimeException("Error connecting to the database: registerDriver()!", e);
+        }
+        try {
             connection = DriverManager.getConnection(
                 config.getUrl(),
                 config.getUser(),
                 config.getPassword()
             );
         } catch (SQLException e) {
-            throw new RuntimeException("Error connecting to the database!", e);
+            throw new RuntimeException("Error connecting to the database: .getConnection()!", e);
         }
     }
 
